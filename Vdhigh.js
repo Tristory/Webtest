bad2831badbbfab8222f1eedd362be094478e163
John Wick <dogeatershalldie@gmail.com>
2021-10-08 16:51:05 +0700

    0

---
+++ Vdhigh.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const hostname = "localhost";
+
+const port = 3000;
+var solan = 0;
+
+const server = http.createServer(
+    (req, res) => {
+        res.statusCode = 200;
+        res.end("Hello !!!" + solan);
+        console.log("--- request !!!", solan++);
+    }
+)
+
+server.listen( port, hostname,
+    () => {
+         console.log("Listen !!!");
+
+    });
